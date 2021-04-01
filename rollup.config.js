4d66562816309ca01c84d5d600c11e3640382379
Satenik Hovsepyan <sathovsepyan@gmail.com>
2021-04-01 12:41:54 +0300

    added rollup bundler

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+
+import typescript from 'rollup-plugin-typescript2';
+import del from 'rollup-plugin-delete';
+import pkg from './package.json';
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'esm' },
+    ],
+    plugins: [
+      del({ targets: ['dist/*', 'playground/src/component-lib'] }),
+      typescript(),
+    ],
+    external: Object.keys(pkg.peerDependencies || {}),
+  },
+];
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { Button } from './components/Button/button';
+import { AreaChart } from './components/AreaChart/areaChart';
+
+export { Button, AreaChart };
