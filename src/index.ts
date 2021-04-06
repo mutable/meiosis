131e05945158373a5ee5a4df34588cb4c4a55fbe
Satenik Hovsepyan <sathovsepyan@gmail.com>
2021-04-06 16:31:01 +0300

    add SystemHealthCard to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { Button } from './components/Button/Button';
 import { AreaChart } from './components/AreaChart/AreaChart';
+import { SystemHealthCard } from './components/SystemHealthCard/SystemHealthCard';
+
 import './styles/global.css'
 
-export { Button, AreaChart };
+export { Button, AreaChart, SystemHealthCard };
