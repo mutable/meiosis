1444853a207021aab079258265a11ae9140d337d
Satenik Hovsepyan <sathovsepyan@gmail.com>
2021-04-09 16:17:48 +0300

    update component templates

--- util/templates/component.types.js
+++ util/templates/component.types.js
@@ -1,7 +1,6 @@
 module.exports = (componentName) => ({
-  content: `// Generated with util/create-component.js
-export interface ${componentName}Props {
-    foo: string;
+  content: `export interface ${componentName}Props {
+  foo: string;
 }
 `,
   extension: `.types.ts`
