c22f00d60cea2504a8b3b5095a1c1fbfaea3aaab
Satenik Hovsepyan <sathovsepyan@gmail.com>
2021-04-14 12:16:58 +0300

    version bump and component exports

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,14 @@ import { Button } from './components/Button/Button';
 import { AreaChart } from './components/AreaChart/AreaChart';
 import { SystemHealthCard } from './components/SystemHealthCard/SystemHealthCard';
 import { Table } from './components/Table/Table';
+import { Badge } from './components/Badge/Badge';
+import { CardList } from './components/CardList/CardList';
+import { DescriptionList } from './components/DescriptionList/DescriptionList';
+import { Icon } from './components/Icon/Icon';
+import { IconLink } from './components/IconLink/IconLink';
+import { Spinner } from './components/Spinner/Spinner';
+import { Card } from './components/Card/Card';
 
 import './styles/global.css'
 
-export { Button, AreaChart, SystemHealthCard, Table };
+export { Button, AreaChart, SystemHealthCard, Table, Badge, Card, CardList, DescriptionList, Icon, IconLink, Spinner };
