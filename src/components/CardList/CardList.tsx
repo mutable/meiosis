e5cd0abe56ce5f90038bc0bf1dc2286b00822230
Satenik Hovsepyan <sathovsepyan@gmail.com>
2021-05-18 15:59:30 +0300

    version bump and CardList css fix

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -4,7 +4,7 @@ import { CardListProps } from "./CardList.types";
 
 export const CardList: React.FC<CardListProps & React.HTMLAttributes<HTMLDivElement>> = ({ items, className, ...props }) => (
   <div className={`${className}`} {...props}>
-    <ul className="grid grid-cols-1 gap-6 md:grid-cols-3">
+    <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
       {
         items.map((item: any, index) => {
           return (
