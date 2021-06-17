6f6502161d0aee103ed37b43c1c069ac3de8d6f4
Satenik Hovsepyan <sathovsepyan@gmail.com>
2021-06-17 20:25:28 +0300

    update badge text-to-color

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,13 +1,17 @@
 export function statusToColor(status: string) {
   if (!status)
     return "blue";
-    
+
   switch (status.toLowerCase()) {
     case "":
     case "active":
     case "normal":
     case "running":
       return "green";
+    case "warning":
+    case "suspended":
+      return "orange";
+    case "error":
     case "critical":
       return "red";
     default:
